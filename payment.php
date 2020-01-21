b5b5f9090d3360535ad05b43ded70ee47610728c
evanthadissanayake <59246360+evanthadissanayake@users.noreply.github.com>
2020-01-21 11:49:49 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -6,12 +6,14 @@ if (strlen($_SESSION['id']==0)) {
     
    
     } 
+
 >>>>>>>>>>>>>>>>>>End of Db connection<<<<<<<<<<<<<<<<<<<<<<<<<
     
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+  <!--content-->
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
